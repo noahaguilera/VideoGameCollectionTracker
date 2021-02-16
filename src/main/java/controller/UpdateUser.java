3af94496f366b08ea2fa0aca854a11aadf701eec
noahaguilera <naguilera@madisoncollege.edu>
2021-02-16 16:52:10 -0600

    user can change their password now

---
+++ src/main/java/controller/UpdateUser.java
@@ -0,0 +1,39 @@
+package controller;
+
+import entity.User;
+import persistence.UserDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * A simple servlet to welcome the user.
+ * @author pwaite
+ */
+
+@WebServlet(
+        urlPatterns = {"/updateUser"}
+)
+
+public class UpdateUser extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserDao userDao = new UserDao();
+        String userName = req.getParameter("userNameInput");
+        String password = req.getParameter("passwordInput");
+        List<User> userUpdate = userDao.getByPropertyEqual("userName", userName);
+        User userUpdated = userUpdate.get(0);
+        userUpdated.setPassword(password);
+        userDao.saveOrUpdate(userUpdated);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/password.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+
+}
