0169748851da85750c1ecc63c6ab77ba7bc72f89
noahaguilera <naguilera@madisoncollege.edu>
2021-02-21 21:38:28 -0600

    All UserDao tests pass

--- src/test/java/persistence/UserDaoTest.java
+++ src/test/java/persistence/UserDaoTest.java
@@ -1,7 +1,11 @@
 package persistence;
 
 import entity.User;
+import test.util.Database;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import javax.servlet.RequestDispatcher;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,38 +18,85 @@ class UserDaoTest {
     UserDao dao;
 
     /**
-     * Creating the dao.
+     * Creating the dao and setting up a clean version of the test database
      */
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
+
+
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+
         dao = new UserDao();
     }
 
     /**
-     * Verifies the method gets all users successfully.
+     * Verifies get all users outputs the correct length successfully.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     void getAllUsersSuccess() {
         List<User> users = dao.getAll();
-        //assertEquals(4, users.size());
+        assertEquals(5, users.size());
     }
 
     /**
-     * Verifies the method gets users by their username successfully.
+     * Verifies the method gets users by their username works successfully.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     void getByUserNameSuccess() {
         List<User> users = dao.getByPropertyLike("userName", "N");
         assertEquals(2, users.size());
     }
 
     /**
-     * Verifies the user is returned correctly based on id search.
+     * Verifies the user is returned correctly based on their id successfully.
      */
-    @org.junit.jupiter.api.Test
+    @Test
     void getIdSuccess() {
         User  retrievedUser = dao.getById(3);
         assertNotNull(retrievedUser);
-        assertEquals("MarvinMartian", retrievedUser.getUserName());
+        assertEquals("NigelThornberry", retrievedUser.getUserName());
+    }
+
+    /**
+     * Verifies the user is added to the database successfully.
+     */
+    @Test
+    void insert() {
+        dao = new UserDao();
+        User newUser = new User("BillNye", "bnye@scienceguy.com", "science",0,  0);
+        dao.insert(newUser);
+        List<User> users = dao.getAll();
+        assertEquals(6, users.size());
+    }
+
+    /**
+     * Verifies user's password has been changed successfully.
+     */
+    @Test
+    void saveOrUpdate() {
+        UserDao userDao = new UserDao();
+        List<User> userUpdate = userDao.getByPropertyEqual("userName", "ElonMusk");
+        User userUpdated = userUpdate.get(0);
+        userUpdated.setPassword("TeslaRules");
+        userDao.saveOrUpdate(userUpdated);
+
+        List<User> test_user = userDao.getByPropertyEqual("password", "TeslaRules");
+        User test_userUpdated = test_user.get(0);
+        assertEquals("ElonMusk", test_userUpdated.getUserName());
+    }
+
+    /**
+     * Verifies user was deleted successfully.
+     */
+    @Test
+    void delete() {
+        UserDao userDao = new UserDao();
+        List<User> user_to_delete = userDao.getByPropertyEqual("userName", "BillGates");
+        User delete_user = user_to_delete.get(0);
+        userDao.delete(delete_user);
+        List<User> deleted_user = userDao.getByPropertyEqual("userName", "BillGates");
+        assertEquals(true , deleted_user.isEmpty());
     }
 }
---
+++ src/test/resources/cleandb.sql
@@ -0,0 +1,2 @@
+delete from user;
+INSERT INTO user VALUES (1,'NoahAguilera','naguilera@madisoncollege.edu','Password123',true),(2,'MarvinMartian','mmartian@mars.com','loonytunes123',false),(3,'NigelThornberry','nthornberry@nickelodeon.com','Password456',false),(4,'BillGates','bgates@microsoft.com','money123',false),(5,'ElonMusk','emusk@tesla.com','tesla123',false);
