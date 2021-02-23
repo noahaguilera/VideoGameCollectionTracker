80f47415a0679c1834d3bbdfed88b32366ddea07
noahaguilera <naguilera@madisoncollege.edu>
2021-02-23 17:14:52 -0600

    Created tests for new DAOs

---
+++ src/test/java/persistence/UserGameListDaoTest.java
@@ -0,0 +1,36 @@
+package persistence;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserGameListDaoTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void saveOrUpdate() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getByPropertyEqual() {
+    }
+
+    @Test
+    void getByPropertyLike() {
+    }
+}
