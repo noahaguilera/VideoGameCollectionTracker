922cbf075ebb970d69421854a581ff6828627180
noahaguilera <naguilera@madisoncollege.edu>
2021-03-07 18:09:18 -0600

    Added game from form is inserted into the gameList database

--- src/main/java/controller/AddGame.java
+++ src/main/java/controller/AddGame.java
@@ -1,5 +1,6 @@
 package controller;
 
+import entity.GameList;
 import entity.User;
 import persistence.GameListDao;
 import persistence.UserDao;
@@ -25,7 +26,21 @@ import java.lang.*;
 public class AddGame extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        GameListDao gameListDao = new GameListDao();
+        String title = req.getParameter("titleInput");
+        String platform = req.getParameter("platformInput");
+        String rating = req.getParameter("ratingInput");
+        String description = req.getParameter("descriptionInput");
+        String date = req.getParameter("releaseDateInput");
+        GameList newGame = new GameList(0, title, platform, rating, description, "", date);
+        try {
+            req.setAttribute("games", gameListDao.insert(newGame));
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/game.jsp");
+            dispatcher.forward(req, resp);
+        } catch (Exception e) {
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/addgame.jsp");
+            dispatcher.forward(req, resp);
+        }
 
 
 
