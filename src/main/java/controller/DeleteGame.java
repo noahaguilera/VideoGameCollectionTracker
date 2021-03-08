f6cc691c4d07119c84ff521768a5ba5d9bbe1f63
noahaguilera <naguilera@madisoncollege.edu>
2021-03-08 01:13:11 -0600

    Updated delete game to also delete game from users gamelist

--- src/main/java/controller/DeleteGame.java
+++ src/main/java/controller/DeleteGame.java
@@ -1,8 +1,8 @@
 package controller;
 
-import entity.GameList;
+import entity.UserGameList;
 import persistence.GameListDao;
-import persistence.UserDao;
+import persistence.UserGameListDao;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * A servlet that deletes a user from the database.
@@ -25,8 +26,14 @@ public class DeleteGame extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         GameListDao gameListDao = new GameListDao();
+        UserGameListDao userGameListDao = new UserGameListDao();
         int id = Integer.parseInt(req.getParameter("trashcan"));
         gameListDao.delete(gameListDao.getById(id));
+        List<UserGameList> deleteUserGame = userGameListDao.getByPropertyEqual("gameList", id);
+        for (int i = 0; i < deleteUserGame.size(); i++) {
+            UserGameList deletedUserGame = deleteUserGame.get(i);
+            userGameListDao.delete(deletedUserGame);
+        }
         RequestDispatcher dispatcher = req.getRequestDispatcher("/game.jsp");
         dispatcher.forward(req, resp);
     }
