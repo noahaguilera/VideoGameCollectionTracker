794f1fcdd4c9ea185b873b125d9182f13e7bebe0
noahaguilera <naguilera@madisoncollege.edu>
2021-03-08 01:12:13 -0600

    Adds users game to their list in the database

---
+++ src/main/java/controller/AddUserGame.java
@@ -0,0 +1,48 @@
+package controller;
+
+import entity.User;
+import entity.UserGameList;
+import persistence.GameListDao;
+import persistence.UserDao;
+import persistence.UserGameListDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.lang.*;
+import java.util.List;
+
+/**
+ * A servlet that adds a user to the database.
+ * @author pwaite
+ */
+
+@WebServlet(
+        urlPatterns = {"/addUserGame"}
+)
+
+public class AddUserGame extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserGameListDao userGameListDao = new UserGameListDao();
+        GameListDao gameListDao = new GameListDao();
+        UserDao userDao = new UserDao();
+        String userName = req.getParameter("userNameInput");
+        int gameId = Integer.parseInt(req.getParameter("idInput"));
+        List<User> userUpdate = userDao.getByPropertyEqual("userName", userName);
+        User userUpdated = userUpdate.get(0);
+        UserGameList newUserGame = new UserGameList(userUpdated, gameListDao.getById(gameId));
+        try {
+            req.setAttribute("games", userGameListDao.insert(newUserGame));
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/usergame.jsp");
+            dispatcher.forward(req, resp);
+        } catch (Exception e) {
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/game.jsp");
+            dispatcher.forward(req, resp);
+        }
+    }
+}
